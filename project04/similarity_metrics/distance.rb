b19de62e3fabd81475fa97b9040c13553b41c3aa
Joe Zeimen <jzeimen@mines.edu>
2011-09-18 00:12:02 -0600

    Completed all distance measurements. Removed falsedata.rb because it was originally there to generate hashes, but then I decide to just use objects and their instance variables instead.

--- project04/similarity_metrics/distance.rb
+++ project04/similarity_metrics/distance.rb
@@ -1,26 +1,27 @@
+# Joe Zeimen
+# Sept 17, 2011
+# Data Mining CSCI568
+#Project 4
 
+# This file contains functions to measure distance between objects.
 
+# Each function looks at 2 objects instance variables and calculates the distance between the 
+# values in those variables in different ways depending on the function. 
 
- # Given 2 hashes with name and number key value pairs it will find the intersection and calculate the 
+ #Given two objects  it will find the intersection and calculate the 
  #normalized euclidean distance between the two.
-def euclideanDistance( dis , dat) 
-	# Finds intersection between the two hashes by giving an array of the keys that are common between the two
-	common_keys = dis.keys & dat.keys
-
-	return 0 if common_keys.empty?
-
-	square_sum = 0
-	common_keys.each do |song|
-		square_sum += (dis[song] - dat[song] ) ** 2
-
+def euclidean_distance( dis , dat) 
+	attributes = dis.instance_variables & dat.instance_variables
+	square_sum =0.0;
+	attributes.each do |inst|															
+			square_sum += (dat.instance_variable_get(inst) - dis.instance_variable_get(inst))**2	
 	end
 	return 1/(1+Math.sqrt(square_sum))
 end
 
-
-/Gives the simple matching coefficient between 2 objects. It looks for matching instance variables
-and compares whether or not they have the same true or false value. The number of matching over the 
-total number of attributes it what SMC calculates/
+# Gives the simple matching coefficient between 2 objects. It looks for matching instance variables
+# and compares whether or not they have the same true or false value. The number of matching over the 
+# total number of attributes it what SMC calculates
 def simple_matching_coefficient( dis, dat)
 	attributes = dis.instance_variables & dat.instance_variables
 	matching_attributes = 0.0
@@ -30,4 +31,82 @@ def simple_matching_coefficient( dis, dat)
 		end
 	end
 	matching_attributes/attributes.length
+end
+
+# Gives the Jaccard coefficient. It is similar to SMC but it does not count instances where both items are 0 or false
+# This is usefull for sparse data 
+def jaccard_coefficient(dis,dat)
+	matching_attributes = 0.0
+	denominator =0 
+	(dis.instance_variables | dat.instance_variables).each do |var| 
+		add_denom = false
+		# If either object has a true value we need to count that in the denominator
+		if 	(dis.instance_variable_defined?(var) && dis.instance_variable_get(var) == true) ||
+			(dat.instance_variable_defined?(var) && dat.instance_variable_get(var) == true)
+			denominator +=1
+		else
+			next #If it doesn't have at least one true we don't care
+		end
+		if (dis.instance_variable_defined?(var) && dat.instance_variable_defined?(var) ) &&
+			 (dis.instance_variable_get(var) == true && dat.instance_variable_get(var) == true)
+			matching_attributes +=1
+		else
+			next #They don't match so we don't add to the numerator
+		end
+	end
+	return 0 if denominator == 0
+	matching_attributes/denominator
+end
+
+#calculates the cosine simularity. This looks at the angle between 2 vectors
+def cosine_similarity(dis,dat)
+	dis_length = Math.sqrt(dotproduct(dis,dis))
+	dat_length = Math.sqrt(dotproduct(dat,dat))
+	(dotproduct(dat,dis)) /(dis_length * dat_length)
+end
+
+#Helper function to calculate the dot product between 2 objects
+#This function uses the object's instance variables
+def dotproduct( first , second)
+	common = first.instance_variables & second.instance_variables
+	sum =0
+	common.each do |inst|
+		sum += first.instance_variable_get(inst) * second.instance_variable_get(inst)
+	end
+	sum
+end
+
+#Calculates the standard deviation of all of the instance variables in the object
+def std_dev(o)
+	average = avg(o)
+	sq_dif = 0.0
+	o.instance_variables.each do |inst| 
+		sq_dif += (o.instance_variable_get(inst)-average)**2
+	end
+	Math.sqrt((1.0/(o.instance_variables.length-1)) * sq_dif)
+end
+
+#calculates the covariance between two object's instance variables
+def covariance( this, that  )
+	this_average = avg(this)
+	that_average = avg(that)
+	sum = 0.0
+	this.instance_variables.each do |inst|
+		sum += (this.instance_variable_get(inst)-this_average)*(that.instance_variable_get(inst)-that_average)
+	end
+	(1.0/(this.instance_variables.length-1)) * sum
+end
+
+#Calculates the average of all of the instance variables in an object
+def avg(o)
+	sum = 0.0
+	o.instance_variables.each do |var|
+		sum += o.instance_variable_get(var)
+	end
+	sum/o.instance_variables.length
+end
+
+#Calculates how linearly dependant between both objects
+def pearson_correlation_coefficient(dis,dat)
+	covariance(dis,dat)/(std_dev(dis)*std_dev(dat))
 end
--- project04/similarity_metrics/testdistance.rb
+++ project04/similarity_metrics/testdistance.rb
@@ -1,25 +1,74 @@
 #! /usr/bin/env ruby
-#Joe Zeimen
-#Data Mining 
-
+# Joe Zeimen
+# Sept 17, 2011
+# Data Mining CSCI568
+#Project 4
 
 require 'distance.rb'
-require 'falseData.rb'
 
-hash = get_song_counts
+class Market_Basket 
+	def initialize(a, b, c, d, e, f, g)
+		@apples 	= a
+		@bananas 	= b
+		@candy 		= c
+		@dates 		= d
+		@eclair 	= e
+		@frosting 	= f
+		@grapes 	= g
+	end
+end
 
-# print_song_counts( hash )
+class Song_Counts
+	def initialize(a, b, c, d, e, f, g)
+		@song1= a
+		@song2= b
+		@song3= c
+		@song4= d
+		@song5= e
+		@song6= f
+		@song7= g
+	end
+end
 
-puts euclideanDistance(hash["Joe"], hash["Andy"])
-
-
-
+#Fake data for market basket type data
 Jack = Market_Basket.new(true,false,true,true,true,false,true)
 Jill = Market_Basket.new(false,false,true,false,true,true,true)
 
+#fake data for other types of data
+Joe = Song_Counts.new(20,22,2,93,4,5,8)
+Andy = Song_Counts.new(50,13,2,44,3,6,2)
 
+# Because all of our functions are basically the same format. Here is a function to test a function. Given the function args
+# expected and function symbol. Also playing with ruby since this is my first time using it
+def test_func(dis, dat, func, expected)
+	actual = send(func,dis, dat)
+	puts (actual == expected  ? "Pass" :  "Fail") + ": " +  func.to_s + " test"
+end
 
-/Testing Simple Matching Coefficient/
+# Testing Euclidean Distance
+expected = 1/(1+Math.sqrt(  (50-20.0)**2 + (22.0-13)**2  + (2.0-2)**2 + (93.0-44)**2 + (4.0-3)**2 +(5.0-6)**2 + (8.0-2)**2 ))
+test_func(Joe, Andy, :euclidean_distance, expected)
+
+# Testing Simple Matching Coefficient
 expected = (4.0/7.0)
-actual = simple_matching_coefficient(Jack, Jill)
-puts   (actual == expected  ? "Pass" :  "Fail") + ": simple_matching_coefficient test"
+test_func(Jack, Jill, :simple_matching_coefficient, expected)
+
+# Testing Jaccard
+expected = (3.0/6)
+test_func(Jack, Jill, :jaccard_coefficient, expected)
+
+# Testing Pearson Correlation Coefficient
+joe_average=(20.0+22+2+93+4+5+8)/7
+andy_average=(50.0+13+2+44+3+6+2)/7
+			#Wrong calculations when return added to break up lines?
+joe_std_dev = Math.sqrt( (1.0/6) *((20-joe_average)**2 + (22-joe_average)**2 + (2-joe_average)**2 + (93-joe_average)**2 +  (4-joe_average)**2 + (5-joe_average)**2 + (8-joe_average)**2 ))
+andy_std_dev = Math.sqrt( (1.0/6) * ((50-andy_average)**2 + (13-andy_average)**2 +(2-andy_average)**2 + (2-andy_average)**2 +  (44-andy_average)**2 + (3-andy_average)**2 + (6-andy_average)**2 ) )
+
+covar = (1.0/6) * (  (20-joe_average)*(50-andy_average) +(22-joe_average)*(13-andy_average) + (2-joe_average)*(2-andy_average) + (93-joe_average)*(44-andy_average) +(4-joe_average)*(3-andy_average) +(5-joe_average)*(6-andy_average) +	(8-joe_average)*(2-andy_average)  )
+expected =  covar/(joe_std_dev*andy_std_dev) #Should be 0.69425792139928 according to google docs
+test_func(Joe, Andy, :pearson_correlation_coefficient, expected)
+
+# Testing Cosine Similarity
+expected = (20*50 + 22*13 + 2*2 + 93*44 + 4*3 + 5*6 + 8*2) / 
+			(  Math.sqrt(20*20 + 22*22 + 2*2 + 93*93 + 4*4 + 5*5 + 8*8) * Math.sqrt(50*50 + 13*13 + 2*2 + 44*44 + 3*3 + 6*6 +2*2)  )
+test_func(Joe, Andy, :cosine_similarity, expected)
