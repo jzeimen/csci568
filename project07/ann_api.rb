1152ecc3b5ec33f55f49f01ebe00773b09972322
Joe Zeimen <jzeimen@gmail.com>
2011-11-07 20:26:41 -0700

    Finished a working version of the ANN and added a readme.

--- project07/ann_api.rb
+++ project07/ann_api.rb
@@ -1,35 +1,34 @@
 class ANN
+
+	#How fasat the ANN "Learns" The higher this is the more 
+	#a single training will influence the ANN range is (0,1)
+	LEARNING_RATE= 0.5
+	#Number of times teh ANN is "trained" when the training method is called
+	TRAINING_ITERATIONS=200
+
 	def initialize(input_size, hidden_layer_size ,output_size)
 		@input = Array.new
 		@hidden_layer = Array.new
 		@output = Array.new
 		@input_weights = Array.new
 		@output_weights = Array.new
+
+		#Create each layer
 		input_size.times {@input.push( Neuron.new )}
 		hidden_layer_size.times { @hidden_layer.push(Neuron.new)}
 		output_size.times{@output.push(Neuron.new)}
 
+		#Create connections between each layer
 		@input.each do |i|
 			@hidden_layer.each do |h|
 				@input_weights.push(Connection.new(h,i))
 			end
 		end
-
-
 		@hidden_layer.each do |h|
 			@output.each do |o|
 				@output_weights.push(Connection.new(o,h))
 			end
 		end
-	end
-
-
-	#Pushes information through the ANN. This function fires each of the neurons
-	#in the input and hidden layers. This then allowes us to pull the results using
-	#get_outputs
-	def feed_forward
-		@input.each {|n| n.fire}
-		@hidden_layer.each {|h| h.fire}
 	end
 
 	def set_inputs(inputs)
@@ -40,12 +39,55 @@ class ANN
 
 	#Maps the array of Nodes to an array of integers 
 	def get_outputs;
+		feed_forward
 		@output.map{|o| o.calculate_output}
 	end
 
-	#
-	def back_propagate
 
+	#Trains the ANN
+	def train (sample_inputs, expected_outputs)
+		set_inputs(sample_inputs)
+		TRAINING_ITERATIONS.times do
+			feed_forward
+			back_propagate(expected_outputs)
+		end
+	end
+
+	
+	private
+
+	#Pushes information through the ANN. This function fires each of the neurons
+	#in the input and hidden layers. This then allowes us to pull the results using
+	#get_outputs
+	def feed_forward
+		@input.each {|n| n.fire}
+		@hidden_layer.each {|h| h.fire}
+	end
+
+	#Using the translated version from collective intelligence 
+	def back_propagate(targets)
+		#For every actual output and its expected output do...
+		[@output, targets].transpose.each do | actual, expected|
+			actual.error =   expected - actual.calculate_output
+			actual.delta = dsigmoid(actual.output) * actual.error
+		end
+
+		#For every hidden node, calculate the error, and delta
+		@hidden_layer.each do |hidden_node|
+			hidden_node.error = 0.0
+			hidden_node.outputs.each do |conn|
+				#error is the running total plus the weight of the connection times the connection's 
+				#output node's delta
+				hidden_node.error= hidden_node.error + conn.weight*conn.output.delta
+			end
+			hidden_node.delta = hidden_node.error * dsigmoid(hidden_node.output)
+		end
+
+		#Update the weights of every connection
+		(@output_weights+@input_weights).each do |conn|
+			change = conn.output.delta*conn.value
+			conn.weight = conn.weight + LEARNING_RATE * change
+		end
 	end
 
 end
@@ -56,6 +98,8 @@ end
 #function
 def sigmoid(number)
 	Math.tanh(number)
+	#Change to number if we want linear (easier to debug)
+	#number
 end
 
 #The derivative of the sigmoid function, Collective Intellegence uses 1-number*number
@@ -63,14 +107,16 @@ end
 #greater than 1 and less than -1, and the sigmoid function is always positive,
 #Thanks to wolframalpha this is the actual derivative
 def dsigmoid(number)
-	(4*Math.cosh(x)^2)/(1+Math.cosh(2*x))^2
+	(4*Math.cosh(number)**2)/(1+Math.cosh(2*number))**2
+	#change return value to 1 if we are doing it linearly like above (easier to debug)
+	#1
 end
 
 
 #Just an object to hold a weight and a value representing the connections between
 #neurons
 class Connection
-	attr_accessor :weight, :value
+	attr_accessor :weight, :value, :output
 	def initialize(output_node, input_node)
 		@output = output_node
 		@input = input_node
@@ -96,7 +142,11 @@ class Neuron
 	def initialize
 		@outputs =Array.new
 		@inputs = Array.new
-		@output=0
+		@output=0.0
+
+		#Info to back propagate
+		@error = 0.0
+		@delta = 0.0
 	end
 
 
@@ -112,6 +162,8 @@ class Neuron
 		@output = sigmoid(weighted_sum)
 	end
 
+	#Also can store information about iteself for back propagation
+	attr_accessor :error, :delta
 
 end
 
--- project07/main.rb
+++ project07/main.rb
@@ -2,8 +2,33 @@
 
 require './ann_api.rb'
 
+INPUT=[1.0,0.25,-0.5]; OUTPUT=[1,-1.0,0.0]
 
+puts "Input"
+puts INPUT
+puts
+puts "Desired output:"
+puts OUTPUT
+puts
+
+
+puts "output before training:"
+#Call ANN Paramaters are the number of nodes in input, hidden and output layers
 my_ann = ANN.new(3,2,3)
-my_ann.set_inputs([-1,1,1])
-my_ann.feed_forward
+my_ann.set_inputs(INPUT)
+
+#prints outputs before training
 puts my_ann.get_outputs
+puts
+
+puts "Output after training:"
+#trains ann
+my_ann.train(INPUT,OUTPUT)
+
+#Prints new traind outputs
+puts my_ann.get_outputs
+puts 
+
+
+puts "Absolute value of difference (desired - calculated)"
+puts [OUTPUT, my_ann.get_outputs].transpose.map{|x,y| (x-y).abs}
