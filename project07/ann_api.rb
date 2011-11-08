c7cf3b4b5d19d54a4354e38d33d27519b8100c70
Joe Zeimen <jzeimen@gmail.com>
2011-11-07 17:28:46 -0700

    Finished all functionality needed to be ready to implement train and back_propagate methods.

--- project07/ann_api.rb
+++ project07/ann_api.rb
@@ -1,8 +1,13 @@
 class ANN
 	def initialize(input_size, hidden_layer_size ,output_size)
+		@input = Array.new
+		@hidden_layer = Array.new
+		@output = Array.new
+		@input_weights = Array.new
+		@output_weights = Array.new
 		input_size.times {@input.push( Neuron.new )}
-		hidden_layer.times { @hidden_layer.push(Neuron.new)}
-		output_size.times{output.push(Neruon.new)}
+		hidden_layer_size.times { @hidden_layer.push(Neuron.new)}
+		output_size.times{@output.push(Neuron.new)}
 
 		@input.each do |i|
 			@hidden_layer.each do |h|
@@ -18,29 +23,62 @@ class ANN
 		end
 	end
 
-	def setInputs(inputs)
-		return if inputs.size != @input.size
-		[inputs,@input].transpose.each {|number,node| node.weight=number}
+
+	#Pushes information through the ANN. This function fires each of the neurons
+	#in the input and hidden layers. This then allowes us to pull the results using
+	#get_outputs
+	def feed_forward
+		@input.each {|n| n.fire}
+		@hidden_layer.each {|h| h.fire}
 	end
 
-	def getOutputs()
+	def set_inputs(inputs)
+		return if inputs.size != @input.size
+		[inputs,@input].transpose.each {|number,node| node.output=number}
+	end
+
+
+	#Maps the array of Nodes to an array of integers 
+	def get_outputs;
 		@output.map{|o| o.calculate_output}
 	end
 
+	#
+	def back_propagate
 
+	end
+
+end
+
+
+#A the function we use to calculate the output of a node,
+#Putting it here lets us swap it out later to decide on the best
+#function
+def sigmoid(number)
+	Math.tanh(number)
+end
+
+#The derivative of the sigmoid function, Collective Intellegence uses 1-number*number
+#I will be using a better approximation since the sum of the weights*values can be
+#greater than 1 and less than -1, and the sigmoid function is always positive,
+#Thanks to wolframalpha this is the actual derivative
+def dsigmoid(number)
+	(4*Math.cosh(x)^2)/(1+Math.cosh(2*x))^2
 end
 
 
 #Just an object to hold a weight and a value representing the connections between
 #neurons
 class Connection
-	attr_accessor: :weight :value
-	def initialize(output, input)
+	attr_accessor :weight, :value
+	def initialize(output_node, input_node)
 		@output = output_node
 		@input = input_node
-		@weight = puts Random.new.rand(-1.0..1.0)
-		input.outputs.push(self)
-		output.inputs.push(self)
+		@weight = Random.new.rand(-1.0..1.0)
+		#for testing and debugging, setting to .5 simply so we know exactly what nubmers should be where
+		#@weight = 0.5
+		@input.outputs.push(self)
+		@output.inputs.push(self)
 	end
 end
 
@@ -54,21 +92,24 @@ end
 #means it is the input layer of neurons.
 #If it does not have any outputs, it means it is the output layer.
 class Neuron
-	attr_accessor: :output
+	attr_accessor :outputs, :inputs, :output
 	def initialize
 		@outputs =Array.new
 		@inputs = Array.new
 		@output=0
 	end
+
+
 	def fire
 		calculate_output
 		@outputs.each {|conn| conn.value = @output}
 	end
 
+	#Takes in data from input connections, and figures out its value
 	def calculate_output
-		return @output if @inputs.count =0
+		return @output if @inputs.count == 0 #When this node is an input node, we shouldn't try and calculate output
 		weighted_sum = @inputs.reduce(0) {|sum, obj| sum+(obj.weight*obj.value)}
-		@output = math.tanh(weighted_sum)
+		@output = sigmoid(weighted_sum)
 	end
 
 
--- project07/main.rb
+++ project07/main.rb
@@ -3,3 +3,7 @@
 require './ann_api.rb'
 
 
+my_ann = ANN.new(3,2,3)
+my_ann.set_inputs([-1,1,1])
+my_ann.feed_forward
+puts my_ann.get_outputs
